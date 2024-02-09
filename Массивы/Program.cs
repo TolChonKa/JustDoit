6a7cf34583ded5d97dae2f6121fc71fdc15a2c02
TolChonKa <gekysik123@gmail.com>
2024-02-09 19:37:39 +0300

    массивы

---
+++ Массивы/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Массивы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите колличество переменных в массиве: ");
+            int a = int.Parse(Console.ReadLine());
+            int[] Array = new int[a];
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write($"Введите число под инексом {i}: ");
+                Array[i] = int.Parse(Console.ReadLine()); 
+            }
+            
+            Console.WriteLine("Вывод массива:");
+            for (int i = Array.Length - 1; i < Array.Length;i--) 
+            {
+                Console.WriteLine(Array[i]);
+            }
+            Console.Read();
+        }
+    }
+}

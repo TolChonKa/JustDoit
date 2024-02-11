e702c6d66751d8f5209ab831bb14c9233ae975c8
Всеволод <gekysik123@gmail.com>
2024-02-11 15:02:06 +0300

    Функции

---
+++ def/def 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace def_2
+{
+    internal class Program
+    {
+        static int Array(int[] myarray, int volue)
+        {
+            for (int i = 0; i < myarray.Length; i++) 
+            { 
+                if (myarray[i] == volue)
+                {
+                    return i;
+                    
+                }
+
+            } 
+            return -1;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество переменных в массиве: ");
+            int a = int.Parse(Console.ReadLine());
+            int[] myarray = new int[a];
+            for (int i = 0; i < myarray.Length; i++) 
+            {
+                Console.Write($"\nВведите число под индексом {i}: ");
+                myarray[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("\nКакое значение вы хотите найти: ");
+            int volue = int.Parse(Console.ReadLine());
+            Console.Write("\nНайденный индекс: ");
+            int k = Array(myarray, volue);
+            Console.Write(k);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ def/def/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace def
+{
+    internal class Program
+    {
+       static void Print(string a, uint b)
+        {
+            for (int i = 0; i < b; i++) 
+            {
+                Console.Write(a);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите символ: ");
+            string simbol = Console.ReadLine();          
+            Console.Write("Введите сколько раз: ");
+            uint Volue = uint.Parse(Console.ReadLine());
+            Console.Write("Вывод: ");
+            Print(simbol, Volue);
+            Console.Read();
+            Console.Read();
+        }
+    }
+}

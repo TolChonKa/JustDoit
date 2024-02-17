0d4a5354cce5217e18fd7c8c3d552f34755f9d63
TolChonKa <gekysik123@gmail.com>
2024-02-17 12:07:34 +0300

    Рекурсия

--- Рекурсия/Program.cs
+++ Рекурсия/Program.cs
@@ -7,9 +7,49 @@ using System.Threading.Tasks;
 namespace Рекурсия
 {
     internal class Program
-    {
+    { 
+        //Вывод суммы всех элементов массива с помощью рекурсии:
+
+       /* static void Massiv(ref int[] newmassiv, int i = 0, int result = 0)
+        {
+            if (i < newmassiv.Length)
+            {
+                result += newmassiv[i];
+                Massiv(ref newmassiv, i + 1, result);
+            }
+            else { Console.WriteLine(result); }
+        }
+        
+
         static void Main(string[] args)
         {
+            int[] massiv = { 1, 2, 3, 4, 5, 6 };
+            Massiv(ref massiv);
+            
+        }*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+       // Вывод суммы цифр любого числа с помощью рекурсии
+        static void sumVolue(int volue, int number = 0, int result = 0)
+        {
+            if (volue != 0)
+            {
+                number = volue % 10;
+                result += number;
+                sumVolue(volue / 10, number, result);
+            }
+            else { Console.WriteLine(result); }
         }
+
+
+        static void Main(string[] args)
+        {
+            int Volue = 0;
+            sumVolue(Volue);
+
+        }
+
+
     }
 }
---
+++ Функция добавления нового элемента в конец массива/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace функция_добавления_нового_элемента_в_конец_массива
+{
+    internal class Program
+    {
+        static void lastMassiv(ref int[] array, int newEl)
+        {
+            int a = array.Length + 1;
+            int[] newArray = new int[a];
+            for (int i = array.Length - 1; i >= 0 ; i--) 
+            {
+                newArray[array.Length] = newEl;
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] myarray = { 1, 2, 3 };
+            lastMassiv(ref myarray, 3);
+
+
+        }
+    }
+}
